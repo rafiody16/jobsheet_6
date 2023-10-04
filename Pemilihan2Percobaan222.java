20bb422ce49ddbe0233f81c9af97e8206d27da41
rafiody16 <rafiody16@gmail.com>
2023-10-04 20:31:12 +0700

    Implementasi Pertanyaan No.1

--- Pemilihan2Percobaan222.java
+++ Pemilihan2Percobaan222.java
@@ -20,9 +20,21 @@ public class Pemilihan2Percobaan222 {
         if (totalSudut == 180)
         {
             if ((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90))
+            {
                 System.out.println("Segitiga Tersebut Adalah Segitiga Siku-Siku");
+            }
+            else if ((sudut1 == sudut2) && (sudut2 ==  sudut3))
+            {
+                System.out.println("Segitiga Tersebut Adalah Segitiga Sama Sisi");
+            }
+            else if ((sudut1 == sudut2) || (sudut2 == sudut3) || (sudut1 == sudut3))
+            {
+                System.out.println("Segitiga Tersebut Adalah Segitiga sama Kaki");
+            }
             else
-                System.out.println("Segitiga Tersebut Bukan Segitiga Siku-Siku");
+            {
+                System.out.println("Segitiga Tersebut Adalah Segitiga Sembarang");
+            }
         }
         else
         {
